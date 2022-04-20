487d210df06dd3a18f39d93a9827379e3782b248
teklu f-a <teklufa@gmail.com>
2022-04-20 15:41:51 +0300

    display records

--- src/components/RecordList/index.js
+++ src/components/RecordList/index.js
@@ -1,13 +1,44 @@
+import { Fragment } from 'react'
 import { useSelector } from 'react-redux'
+
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 const RecordList = (props) => {
 
   const { records } = useSelector(state => state.record)
 
   return (
-    <>
-      list of records      
-    </>
+    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+      <>
+        <Box sx={{p: 1,  display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', bgcolor: '#f4f4f4' }}>
+          <Typography>Name</Typography>
+          <Typography>Email</Typography>
+        </Box>
+        <Box sx={{p: 1, display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' , bgcolor: '#f4f4f4'}}>
+          <Typography>Age</Typography>
+          <Typography>Gender</Typography>
+        </Box>
+        <Box sx={{ bgcolor: '#f4f4f4', p: 1}}><Typography>Actions</Typography></Box>
+      </>
+      { records.map(record => ( 
+        <Fragment key={record._id}>
+          <Box  sx={{ p: 1, display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+            <Typography>{record.name}</Typography>
+            <Typography>{record.email}</Typography>
+          </Box>
+          <Box  sx={{p: 1,  display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+            <Typography>{record.age}</Typography>
+            <Typography>{record.gender}</Typography>
+          </Box>
+          <Box  sx={{p: 1, display: 'flex' }}>
+            <Button size='small'>edit</Button>
+            <Button size='small'>delete</Button>
+          </Box>
+        </Fragment>
+      ))}
+    </Box>
   );
 };
 
